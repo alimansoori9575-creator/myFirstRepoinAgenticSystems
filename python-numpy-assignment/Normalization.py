7dfd8ae5135e697649779dc7c2448dac885c546a
alimansoori9575-creator <alimansoori9575@gmail.com>
2026-02-19 19:46:38 +0530

    NumPy-Based Data Normalization Pipeline

--- python-numpy-assignment/Normalization.py
+++ python-numpy-assignment/Normalization.py
@@ -5,9 +5,15 @@ data = np.array([10, 20, 30, 40])
 
 mean = data.mean()
 
-std = data.mean()
+std = data.std()
 
 normalized = (data - mean) / std
 
-reshaped = normalized.reshaped(2, 2)
+reshaped = normalized.reshape(2, 2)
+print('Original data:', data)
+print('Mean:', mean)
+print('Standard Deviation:', std)
+print('Normalized data:', normalized)
+print('Reshaped data:', reshaped)
+print('Reshaped data shape:', reshaped.shape)
 
