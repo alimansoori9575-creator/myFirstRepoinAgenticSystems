336f2927a72ba8da80f223f0d77758d6d51319a0
alimansoori9575-creator <alimansoori9575@gmail.com>
2026-03-02 23:20:19 +0530

    i deleted them

--- python-data-assignment_pandas/dataset_inspection.py
+++
@@ -1,4 +0,0 @@
-###         Question: AI Dataset Inspection Pipeline         ###
-import pandas as pd
-df = pd.read_csv(r"C:\Users\91917\Desktop\Module 1\myFirstRepoinAgenticSystems\python-data-assignment_pandas\employee.csv")
-print(df)
